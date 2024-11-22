f038fe628b720478d19b52920905116467666376
Taninokuchi-Y <taninokuchi.yuki@gmail.com>
2024-11-22 17:19:43 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,            presence: true
-  validates :email,               presence: true
-  validates :encrypted_password,  presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }, allow_nil: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   validates :last_name,           presence: true
   validates :first_name,          presence: true
   validates :last_name_kana,      presence: true
   validates :first_name_kana,     presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters." }
+  validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters." }
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters." }
   validates :birth_date,          presence: true
 
-  has_many :items
-  has_many :purchases
+  # has_many :items
+  # has_many :purchases
 end
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_19_191121) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
-    t.string "password", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "last_name", default: "", null: false
     t.string "first_name", default: "", null: false
     t.string "last_name_kana", default: "", null: false
