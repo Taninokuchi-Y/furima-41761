4dda1672d3c31168f0775bf2046d5372b9b725b5
Taninokuchi-Y <taninokuchi.yuki@gmail.com>
2024-11-22 17:52:46 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2024_11_19_191121) do
-  create_table "items", charset: "utf8mb3", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
