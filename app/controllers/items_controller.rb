075afa1041e2cb36aca3975cabe24e6880df3fa3
Taninokuchi-Y <taninokuchi.yuki@gmail.com>
2024-11-27 19:11:40 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,8 +23,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item), notice: '商品情報が更新されました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
   # def destroy
   # end
@@ -34,6 +45,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :item_category_id, :item_condition_id, 
                                  :item_postage_id, :prefecture_id, :item_shipping_time_id, :price, :image)
           .merge(user_id: current_user.id)
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    if @item.user != current_user
+      redirect_to root_path, alert: "他のユーザーの商品は編集できません。"
+    end
   end
   
 end
